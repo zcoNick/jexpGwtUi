5c90e4c3e4513532d45cc4ca24d12fd015e1e0bb
Hakan Cunier <hakan.cunier@javexpress.com>
2015-05-11 15:31:32 +0300

    Open model refactorings

--- src/com/javexpress/gwt/library/ui/ClientContext.java
+++ src/com/javexpress/gwt/library/ui/ClientContext.java
@@ -2,6 +2,7 @@ package com.javexpress.gwt.library.ui;
 
 import java.util.List;
 
+import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
@@ -11,23 +12,23 @@ import com.javexpress.gwt.library.shared.nls.CommonResources;
 import com.javexpress.gwt.library.ui.form.IUIComposite;
 import com.javexpress.gwt.library.ui.js.JexpCallback;
 
-public interface ClientContext {
+public abstract class ClientContext implements EntryPoint {
 
 	public static ClientContext		instance	= null;
 	public static CommonResources	nlsCommon	= GWT.create(CommonResources.class);
 
-	void formYetkiListesi(Long moduleId, String authKey, JexpCallback<List<String>> callback);
+	abstract public void formYetkiListesi(Long moduleId, String authKey, JexpCallback<List<String>> callback);
 
-	String getModuleNls(Long moduleId, String key);
+	abstract public String getModuleNls(Long moduleId, String key);
 
-	void createJiraIssue(long moduleId, String summary, String description, String userAgent, byte issueType, AsyncCallback<Result<String>> callback);
+	abstract public void createJiraIssue(long moduleId, String summary, String description, String userAgent, byte issueType, AsyncCallback<Result<String>> callback);
 
-	void openHelp(IUIComposite widget);
+	abstract public void openHelp(IUIComposite widget);
 
-	void goLockScreen();
+	abstract public void goLockScreen();
 
-	void showError(String windowId, AppException ae);
+	abstract public void showError(String windowId, AppException ae);
 
-	ServiceDefTarget getModuleServiceTarget(long moduleId);
+	abstract public ServiceDefTarget getModuleServiceTarget(long moduleId);
 
 }
