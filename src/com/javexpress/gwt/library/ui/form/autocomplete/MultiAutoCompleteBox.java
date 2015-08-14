700c42c33b75d9f0dc0e8036ec3b6bcf9391a5db
Hakan Cunier <hakan.cunier@javexpress.com>
2015-08-14 15:43:54 +0300

    MultiAutoCompleteBox

--- src/com/javexpress/gwt/library/ui/form/autocomplete/MultiAutoCompleteBox.java
+++ src/com/javexpress/gwt/library/ui/form/autocomplete/MultiAutoCompleteBox.java
@@ -8,14 +8,11 @@ import java.util.List;
 import java.util.Map;
 
 import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.InputElement;
 import com.google.gwt.dom.client.Style.Display;
-import com.google.gwt.dom.client.Style.FontWeight;
 import com.google.gwt.dom.client.Style.Overflow;
+import com.google.gwt.dom.client.Style.TextAlign;
 import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.dom.client.TableElement;
 import com.google.gwt.dom.client.TableRowElement;
 import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -38,9 +35,8 @@ import com.javexpress.gwt.library.ui.js.JsonMap;
 public class MultiAutoCompleteBox extends AbstractContainer implements IUserInputWidget<ArrayList<String>> {
 
 	private boolean						required;
-	private TableElement				table;
-	private InputElement				input;
-	private DivElement					div;
+	private Element						input;
+	private Element						items;
 	private IMultiAutoCompleteListener	listener;
 	private JsonMap						options;
 	private Map<String, JsonMap>		acData;
@@ -62,53 +58,52 @@ public class MultiAutoCompleteBox extends AbstractContainer implements IUserInpu
 	}
 
 	public MultiAutoCompleteBox(Widget parent, String id, String searchText, boolean fitToParent, final IJsonServicePoint servicePoint) {
-		super(DOM.createTable());
+		super(DOM.createDiv());
 		JsUtil.ensureId(parent, this, WidgetConst.MULTIAUTOCOMPLETE_PREFIX, id);
-
-		addStyleName("jexpBorderBox");
+		getElement().addClassName((JsUtil.USE_BOOTSTRAP ? "" : "ui-widget ui-widget-content ui-corner-all ") + "jexpMultiAutoCompleteBox");
 
 		if (fitToParent) {
 			getElement().getStyle().setWidth(100, Unit.PCT);
 			getElement().getStyle().setHeight(100, Unit.PCT);
 		}
 
-		table = getElement().cast();
-		table.addClassName("ui-widget ui-widget-content ui-corner-all jesMultiAutoCompleteBox");
-		table.setCellPadding(2);
-		table.setCellSpacing(0);
-
-		Element tr = DOM.createTR();
-		tr.setAttribute("height", "1%");
-		tr.addClassName("ui-widget-header");
-		tr.getStyle().setFontWeight(FontWeight.NORMAL);
-		Element td = DOM.createTD();
-		td.setAttribute("align", "right");
-		Element label = DOM.createSpan();
-		label.setInnerText(searchText + " : ");
-		td.appendChild(label);
-		tr.appendChild(td);
-		td = DOM.createTD();
-		input = DOM.createInputText().cast();
-		input.getStyle().setWidth(97, Unit.PCT);
+		Element tr = DOM.createDiv();
+		tr.addClassName("col-xs-12");
+		int xs = 12;
+		if (searchText != null) {
+			Element td = DOM.createDiv();
+			td.setClassName("col-xs-3");
+			Element label = DOM.createSpan();
+			label.setInnerText(searchText + " : ");
+			tr.appendChild(td);
+			xs -= 3;
+		}
+		Element inpCont = DOM.createDiv();
+		inpCont.addClassName("input-group jexpAutoComplete");
+		input = DOM.createInputText();
+		input.addClassName("col-xs-" + xs);
 		JsUtil.ensureSubId(getElement(), input, WidgetConst.AUTOCOMPLETE_PREFIX);
-		td.appendChild(input);
-		tr.appendChild(td);
-		tdOpen = DOM.createTD();
-		tr.appendChild(tdOpen);
-		table.appendChild(tr);
+		inpCont.appendChild(input);
+		Element span = DOM.createSpan();
+		span.setClassName("input-group-addon jexpHandCursor");
+		Element icon = DOM.createElement("i");
+		icon.setClassName("fa fa-search");
+		span.appendChild(icon);
+		inpCont.appendChild(span);
+		tr.appendChild(inpCont);
 
-		tr = DOM.createTR();
-		tr.setAttribute("height", "99%");
-		td = DOM.createTD();
-		td.setAttribute("colspan", "3");
-		td.setAttribute("height", "100%");
-		div = DOM.createDiv().cast();
-		div.getStyle().setWidth(100, Unit.PCT);
-		div.getStyle().setHeight(100, Unit.PCT);
-		div.getStyle().setOverflow(Overflow.AUTO);
-		td.appendChild(div);
-		tr.appendChild(td);
-		table.appendChild(tr);
+		getElement().appendChild(tr);
+
+		Element itemsDiv = DOM.createDiv();
+		itemsDiv.addClassName("col-xs-12");
+		itemsDiv.getStyle().setHeight(100, Unit.PCT);
+		itemsDiv.getStyle().setOverflow(Overflow.AUTO);
+		Element itemsTable = DOM.createTable();
+		itemsTable.addClassName("table table-striped");
+		items = DOM.createTBody();
+		itemsTable.appendChild(items);
+		itemsDiv.appendChild(itemsTable);
+		getElement().appendChild(itemsDiv);
 
 		options = new JsonMap();
 		options.setInt("minLength", 2);
@@ -147,7 +142,7 @@ public class MultiAutoCompleteBox extends AbstractContainer implements IUserInpu
 			createByJs(this, input, options.getJavaScriptObject(), listener != null);
 	}
 
-	private native void createByJs(MultiAutoCompleteBox x, InputElement element, JavaScriptObject options, boolean hasListener) /*-{
+	private native void createByJs(MultiAutoCompleteBox x, Element element, JavaScriptObject options, boolean hasListener) /*-{
 		var el = $wnd.$(element);
 		if (hasListener) {
 			options.source = function(request, response) {
@@ -177,36 +172,52 @@ public class MultiAutoCompleteBox extends AbstractContainer implements IUserInpu
 
 	@Override
 	protected void onUnload() {
+		destroyByJs(input, items);
 		options = null;
 		input = null;
-		div = null;
-		table = null;
+		items = null;
 		acData = null;
 		if (btOpen != null)
 			remove(btOpen);
 		tdOpen = null;
 		btOpen = null;
-		destroyByJs(input);
 		super.onUnload();
 	}
 
-	private native void destroyByJs(InputElement element) /*-{
+	private native void destroyByJs(Element element, Element items) /*-{
 		$wnd.$(element).jexpautocomplete('destroy');
+		$wnd.$(items).empty().off();
 	}-*/;
 
 	@Override
 	public ArrayList<String> getValue() {
 		ArrayList<String> list = new ArrayList<String>();
-		for (int i = 0; i < div.getChildCount(); i++) {
-			Element t = (Element) div.getChild(i);
+		for (int i = 0; i < items.getChildCount(); i++) {
+			Element t = (Element) items.getChild(i);
 			list.add(t.getAttribute("value"));
 		}
 		return list.isEmpty() ? null : list;
 	}
 
+	public void setValue(ArrayList<? extends Serializable> ids) {
+		removeItems();
+		if (ids == null || ids.isEmpty())
+			return;
+		StringBuilder values = new StringBuilder();
+		boolean first = true;
+		for (Serializable id : ids) {
+			if (!first)
+				values.append(";");
+			else
+				first = false;
+			values.append(id.toString());
+		}
+		_setValueByIds(this, options.getJavaScriptObject(), values.toString());
+	}
+
 	private Element findElement(String id) {
-		for (int i = 0; i < div.getChildCount(); i++) {
-			Element t = (Element) div.getChild(i);
+		for (int i = 0; i < items.getChildCount(); i++) {
+			Element t = (Element) items.getChild(i);
 			if (id.equals(t.getAttribute("value")))
 				return t;
 		}
@@ -215,19 +226,19 @@ public class MultiAutoCompleteBox extends AbstractContainer implements IUserInpu
 
 	public List<Long> getValuesLong() {
 		List<Long> list = new ArrayList<Long>();
-		for (int i = 0; i < div.getChildCount(); i++) {
-			Element t = (Element) div.getChild(i);
+		for (int i = 0; i < items.getChildCount(); i++) {
+			Element t = (Element) items.getChild(i);
 			list.add(JsUtil.asLong(t.getAttribute("value")));
 		}
 		return list.isEmpty() ? null : list;
 	}
 
-	private native void _addCloseEvent(MultiAutoCompleteBox x, Element span) /*-{
+	private native void _addDeleteEvent(MultiAutoCompleteBox x, Element span) /*-{
 		$wnd
 				.$(span)
 				.click(
 						function() {
-							var item = $wnd.$(this).parent().parent().parent();
+							var item = $wnd.$(this).parent().parent();
 							var val = item.attr("value");
 							item.fadeOut(300, function() {
 								$wnd.$(this).remove();
@@ -284,6 +295,22 @@ public class MultiAutoCompleteBox extends AbstractContainer implements IUserInpu
 		return acData.get(val);
 	}
 
+	private native void _setValueByIds(MultiAutoCompleteBox x, JavaScriptObject options, String values) /*-{
+		$wnd.$
+				.post(
+						options.url,
+						{
+							"term" : "@" + values
+						},
+						function(data) {
+							if (!data)
+								return;
+							for (var i = 0; i < data.length; i++) {
+								x.@com.javexpress.gwt.library.ui.form.autocomplete.MultiAutoCompleteBox::fireAddToSelection(Ljava/lang/String;Ljava/lang/String;Lcom/google/gwt/core/client/JavaScriptObject;Z)(data[i].id,data[i].label,data[i].data,true);
+							}
+						}, "json");
+	}-*/;
+
 	// ---------- EVENTS
 	private boolean fireOnSelect(final String id, final String label, JavaScriptObject data) throws Exception {
 		Element existing = findElement(id);
@@ -307,17 +334,12 @@ public class MultiAutoCompleteBox extends AbstractContainer implements IUserInpu
 	}
 
 	private void fireAddToSelection(final String id, final String label, JavaScriptObject data, boolean userAction) {
-		TableElement t = DOM.createTable().cast();
-		t.setAttribute("align", "center");
-		t.addClassName("jexpMultiAutoCompleteListItem");
-		t.setCellPadding(2);
-		t.setCellSpacing(0);
-		t.getStyle().setDisplay(Display.NONE);
-		t.getStyle().setWidth(99, Unit.PCT);
-		t.setAttribute("value", id);
 		TableRowElement tr = DOM.createTR().cast();
+		tr.getStyle().setDisplay(Display.NONE);
+		tr.addClassName("jexpMultiAutoCompleteListItem");
+		tr.setAttribute("value", id);
 		TableRowElement td = DOM.createTD().cast();
-		td.setAttribute("width", "98%");
+		td.setClassName("col-xs-11");
 		Element child = null;
 		JsonMap jsonmapdata = data == null ? null : new JsonMap(data);
 		if (listener != null)
@@ -329,21 +351,21 @@ public class MultiAutoCompleteBox extends AbstractContainer implements IUserInpu
 		td.appendChild(child);
 		tr.appendChild(td);
 		td = DOM.createTD().cast();
-		td.setAttribute("width", "1%");
+		td.setClassName("col-xs-1");
+		td.getStyle().setTextAlign(TextAlign.CENTER);
 		Element span = DOM.createSpan();
-		span.addClassName("ui-icon ui-icon-only ui-icon-trash ui-cursor-hand");
-		_addCloseEvent(this, span);
+		span.addClassName("fa fa-minus jexpHandCursor");
+		_addDeleteEvent(this, span);
 		td.appendChild(span);
 		tr.appendChild(td);
-		t.appendChild(tr);
-		div.insertFirst(t);
+		items.insertFirst(tr);
 		if (data != null) {
 			if (acData == null)
 				acData = new HashMap<String, JsonMap>();
 			acData.put(id, jsonmapdata);
 		}
 		fireOnAdd(id, jsonmapdata, userAction);
-		JsUtil.showElement(t, JqEffect.slide);
+		JsUtil.showElement(tr, JqEffect.highlight);
 	}
 
 	private void fireOnSearch(JavaScriptObject postData) {
@@ -352,8 +374,8 @@ public class MultiAutoCompleteBox extends AbstractContainer implements IUserInpu
 	}
 
 	public void removeItems() {
-		for (int i = 0; i < div.getChildCount(); i++) {
-			Element item = (Element) div.getChild(i);
+		for (int i = 0; i < items.getChildCount(); i++) {
+			Element item = (Element) items.getChild(i);
 			String val = item.getAttribute("value");
 			if (acData != null)
 				acData.remove(val);
