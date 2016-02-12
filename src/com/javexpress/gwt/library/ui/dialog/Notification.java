6a5fac4c08a45eafd81ba47c4c7fcfd3ecdd08e9
Hakan Cunier <hakan.cunier@gmail.com>
2016-02-12 14:32:06 +0200

    AppException fix

--- src/com/javexpress/gwt/library/ui/dialog/Notification.java
+++ src/com/javexpress/gwt/library/ui/dialog/Notification.java
@@ -1,7 +1,9 @@
 package com.javexpress.gwt.library.ui.dialog;
 
 import com.google.gwt.core.client.GWT;
+import com.javexpress.common.model.item.exception.AppException;
 import com.javexpress.gwt.library.ui.ClientContext;
+import com.javexpress.gwt.library.ui.facet.ProvidesModuleUtils;
 import com.javexpress.gwt.library.ui.js.JsUtil;
 
 public class Notification {
@@ -11,17 +13,24 @@ public class Notification {
 	}
 
 	public static int showNotification(final String pSummary, final String pDescription, final NotificationType type) {
-		return showNotification(pSummary, pDescription, type, type == NotificationType.error || type == NotificationType.fatal,
-				type == NotificationType.warning ? 3500 : 2000);
+		return showNotification(pSummary, pDescription, type, type == NotificationType.error || type == NotificationType.fatal, type == NotificationType.warning ? 3500 : 2000);
+	}
+
+	public static int showError(AppException appException) {
+		AppException appEx = appException;
+		String s = (appEx).getErrorCode();
+		if (ClientContext.instance instanceof ProvidesModuleUtils)
+			s = ((ProvidesModuleUtils) ClientContext.instance).getModuleNls(appEx.getModuleId(), "error_" + appEx.getErrorCode());
+		else
+			s = appEx.getErrorCode();
+		return Notification.showNotification(ClientContext.nlsCommon.hata(), s, NotificationType.error);
 	}
 
 	public static int showNotification(final String pSummary, final String pDescription, final NotificationType type, final boolean sticky, final int time) {
 		if (JsUtil.USE_BOOTSTRAP) {
-			return showNotificationBS_(pSummary, pDescription, GWT.getModuleBaseURL() + "scripts/gritter/" + type.toString() + ".png", sticky, time,
-					type == NotificationType.error || type == NotificationType.fatal, type.toString());
+			return showNotificationBS_(pSummary, pDescription, GWT.getModuleBaseURL() + "scripts/gritter/" + type.toString() + ".png", sticky, time, type == NotificationType.error || type == NotificationType.fatal, type.toString());
 		} else
-			return showNotification_(pSummary, pDescription, GWT.getModuleBaseURL() + "scripts/gritter/" + type.toString() + ".png", sticky, time,
-					type == NotificationType.error || type == NotificationType.fatal);
+			return showNotification_(pSummary, pDescription, GWT.getModuleBaseURL() + "scripts/gritter/" + type.toString() + ".png", sticky, time, type == NotificationType.error || type == NotificationType.fatal);
 	}
 
 	private static native int showNotification_(final String pSummary, final String pDescription, final String pImage, final boolean sticky, final int time, boolean dark) /*-{
