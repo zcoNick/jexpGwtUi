64c6abb3fc19b8efeb898a4e6b2c82c07722ab26
Hakan Cunier <hakan.cunier@gmail.com>
2016-02-12 11:55:19 +0200

    Jira fix

--- src/com/javexpress/gwt/library/ui/facet/ProvidesJira.java
+++ src/com/javexpress/gwt/library/ui/facet/ProvidesJira.java
@@ -1,11 +1,11 @@
 package com.javexpress.gwt.library.ui.facet;
 
-import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.javexpress.common.model.item.Result;
+import com.javexpress.gwt.library.ui.js.JsonMap;
 
 public interface ProvidesJira {
 
-	void createJiraIssue(long moduleId, String summary, String description, String userAgent, byte issueType, JSONObject customValues, AsyncCallback<Result<String>> callback);
+	void createJiraIssue(long moduleId, String summary, String description, String userAgent, byte issueType, JsonMap customValues, AsyncCallback<Result<String>> callback);
 
 }
