cd3a5345a7544f6b5b25f98cf8750b2a010e0129
Hakan Cunier <hakan.cunier@gmail.com>
2016-06-16 13:18:17 +0300

    SimplePanel

--- src/com/javexpress/gwt/library/ui/bootstrap/FormGroupCell.java
+++ src/com/javexpress/gwt/library/ui/bootstrap/FormGroupCell.java
@@ -1,12 +1,11 @@
 package com.javexpress.gwt.library.ui.bootstrap;
 
-import com.google.gwt.user.client.DOM;
 import com.javexpress.gwt.library.ui.container.panel.SimplePanel;
 
 public class FormGroupCell extends SimplePanel {
 
 	public FormGroupCell() {
-		super(DOM.createDiv());
+		super();
 		setStyleName("form-group");
 	}
 
